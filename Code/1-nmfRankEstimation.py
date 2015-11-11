3744e61bd5e301afbaf22f0ddb0ea6551b1d7ece
Yan Tan <yan.tan@tamr.com>
2015-11-11 13:50:58 -0500

    modified the user interface

--- Code/1-nmfRankEstimation.py
+++ Code/1-nmfRankEstimation.py
@@ -43,21 +43,19 @@ if __name__ == "__main__":
                     help="maximum rank of data to be estimated")
 
   (options, args) = parser.parse_args()
-  file_name = options.filename
+  filename = options.filename
   nrun = int(options.nrun)
   start = int(options.start)
   end = int(options.end)
 
-  data = read_data(file_name)
+  current_path = os.path.dirname(os.path.realpath(__file__))
+  parent_path = os.path.dirname(current_path)
+
+  data = read_data(os.path.join(parent_path, 'Data', filename))
   print data.shape
   print 'The dimension of data is: ' + str(data.shape[0]) + ' ' + str(data.shape[1])
 
   res_list = []
-
-  # dirname = os.path.split(file_name)[0]
-  # parent_dir = os.path.split(dirname)[0]
-  # parent_dir = os.path.split(parent_dir)[0]
-  # sub_folder = os.path.split(dirname)[1]
 
   for i in range(nrun):
      res = factor_eval(data.T, ranks=range(start, end+1), method="nmf", max_iter=1000, nrun=20)
@@ -65,9 +63,9 @@ if __name__ == "__main__":
      plt.plot(range(start , end+1), res, 'r--', range(start, end+1), res, 'bo')
      plt.ylabel("cophenetic coefficient")
      plt.xlabel("Factors")
-     plt.savefig(os.path.join("..", "Result") + "/" + os.path.basename(file_name) + "." + str(i) + '.pdf') 
+     plt.savefig(os.path.join(parent_path, "Result", os.path.basename(filename) + "." + str(i) + '.pdf'))
 
-  with open(os.path.join("..", "Result") + "/" + os.path.basename(file_name) + "." + "cophentic" + ".txt", 'w') as f:
+  with open(os.path.join(parent_path, "Result", os.path.basename(filename) + "." + "cophentic" + ".txt"), 'w') as f:
      for res in res_list:
         f.write("\t".join(map(lambda x : str(x), res)))
         f.write("\n")    
--- Code/2-nmfComputeW.py
+++ Code/2-nmfComputeW.py
@@ -7,6 +7,8 @@ import nimfa
 import sys
 import os
 
+from optparse import OptionParser
+
 def read_data(filename):
 	data_array = []
 	with open(filename, 'r') as f:
@@ -31,10 +33,6 @@ def get_gene_names(filename):
 def save_data(data, gene_names, filename):
    dim = data.shape
    with open(filename, 'w') as f:
-      # write header
-      #f.write("#1.2\n")
-      #f.write("%s\t%s\n" % dim)
-      #f.write("Name\tfactor1\tfactor2\tfactor3\tfactor4\tfactor5\n")
       f.write("Name")
       for i in range(dim[1]):
          f.write("\t"+"factor"+str(i+1))
@@ -57,6 +55,9 @@ def factor_eval(data, ranks, nrun=40, method="nmf", max_iter=2000):
         coefs.append(coef)
     return coefs
 
+def compute_result_filename(filename, rank):
+  return os.path.basename(filename) +  '.rank.' + str(rank) + '.W.txt'
+
 def compute_w(data, rank, method="nmf", max_iter=2000):
    fctr = nimfa.mf(data, method=method, max_iter=max_iter, rank=rank)
    fctr_res = nimfa.mf_run(fctr)
@@ -66,23 +67,20 @@ def compute_w(data, rank, method="nmf", max_iter=2000):
 if __name__ == "__main__":
    parser = OptionParser()
    parser.add_option("-f", "--file", dest="filename",
-                help="gct file used for rank estimation", metavar="FILE")
+                help="gct file used for W matrix computation", metavar="FILE")
    parser.add_option("-r", "--rank", dest="rank",
                   help="number of columns for W matrix", metavar="NRUN")
 
    (options, args) = parser.parse_args()
 
-   file_name = options.filename
-   rank = options.rank
+   filename = options.filename
+   rank = int(options.rank)
 
-   data = read_data(file_name)
+   current_path = os.path.dirname(os.path.realpath(__file__))
+   parent_path = os.path.dirname(current_path)
+   data = read_data(os.path.join(parent_path, 'Data', filename))
    print data.shape
 
-   dirname = os.path.split(file_name)[0]
-   parent_dir = os.path.split(dirname)[0]
-   parent_dir = os.path.split(parent_dir)[0]
-   sub_folder = os.path.split(dirname)[1]
-
    res = compute_w(data.T, rank=rank, method="nmf", max_iter=1000)
-   gene_names = get_gene_names(file_name)
-   save_data(res, gene_names, os.path.join(parent_dir, "Result", sub_folder) + "/" + os.path.basename(file_name) +  '.rank.' + str(rank) + '.W.txt') 
+   gene_names = get_gene_names(os.path.join(parent_path, 'Data', filename))
+   save_data(res, gene_names, os.path.join(parent_path, "Result", compute_result_filename(filename, rank))) 
